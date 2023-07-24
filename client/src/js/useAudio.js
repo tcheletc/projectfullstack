7292f63b1b01e5d0afb2ebbf9c2de7f2c7a41946
תכלת כהן <tccohen@g.jct.ac.il>
2023-07-24 18:46:30 +0300

    add audio

---
+++ client/src/js/useAudio.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+const useAudio = url => {
+    const [audio] = useState(new Audio(url));
+    const [playing, setPlaying] = useState(false);
+    
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+      },
+      [playing]
+    );
+  
+    useEffect(() => {
+      audio.addEventListener('ended', () => setPlaying(false));
+      return () => {
+        audio.removeEventListener('ended', () => setPlaying(false));
+      };
+    }, []);
+  
+    return [playing, setPlaying];
+};
+
+export default useAudio;
+  
